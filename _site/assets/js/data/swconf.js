424f27eb46658bcfee9ba46562a07692ee2c2fbf
ian <xonic789@naver.com>
2024-04-01 13:32:44 +0900

    테스트 글 작성

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1711945121',resources: [
+    cacheName: 'chirpy-1711945941',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
