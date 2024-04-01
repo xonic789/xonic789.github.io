d2b67385c05f1fb48104a999cb5a2171048d472a
ian <xonic789@naver.com>
2024-04-01 13:20:57 +0900

    change bundle

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1711761018',resources: [
+    cacheName: 'chirpy-1711945121',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
