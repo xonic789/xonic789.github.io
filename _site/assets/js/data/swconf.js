a12631dc97070adaf24350012749f5b0ed1175d7
ian <xonic789@naver.com>
2024-04-01 13:38:24 +0900

    create first post

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1711945941',resources: [
+    cacheName: 'chirpy-1711946262',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
