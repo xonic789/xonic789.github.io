f2bf6454a01e4ac47611dfac0928e434db373adb
ian <xonic789@naver.com>
2024-04-10 23:55:11 +0900

    옛날글 옮겨오기 - 토이프로젝트 회고

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1712760190',resources: [
+    cacheName: 'chirpy-1712760897',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
