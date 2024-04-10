cb9985c1bc5e738ad765e5b878b7ef692b88765d
ian <xonic789@naver.com>
2024-04-10 23:44:45 +0900

    빌드 수정 사항

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1712760082',resources: [
+    cacheName: 'chirpy-1712760190',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
