97e841fcd31751039f874bc3691501134df43d14
ian <xonic789@naver.com>
2024-04-12 13:08:04 +0900

    google verification 추가

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1712760897',resources: [
+    cacheName: 'chirpy-1712894863',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
