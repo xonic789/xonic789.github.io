68800ff967c686bf14398fa77cd98eab5b92c91b
ian <xonic789@naver.com>
2024-04-15 21:52:28 +0900

    소프트웨어 장인 정신 글 내용 추가

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1712894863',resources: [
+    cacheName: 'chirpy-1713183031',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
