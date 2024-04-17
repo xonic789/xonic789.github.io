26d3de651612bee2eacbd1560bc05d829b80af92
ian <xonic789@naver.com>
2024-04-18 00:41:28 +0900

    소프트웨어 장인 책 경로 추가

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1713368333',resources: [
+    cacheName: 'chirpy-1713368446',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
