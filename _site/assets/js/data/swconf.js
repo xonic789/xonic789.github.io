9a75dda4e0db901c27fec6aefd490d318ba65ccd
xonic <xonic789@naver.com>
2024-05-16 16:26:32 +0900

    스타트업에 대한 나의 생각

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1714997180',resources: [
+    cacheName: 'chirpy-1715844136',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
