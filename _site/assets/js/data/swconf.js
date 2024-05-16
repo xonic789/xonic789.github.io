4df3b74c5432a825ab2a30e098d4f612585b922e
xonic <xonic789@naver.com>
2024-05-16 16:29:55 +0900

    add gitignore

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1715844457',resources: [
+    cacheName: 'chirpy-1715844576',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
