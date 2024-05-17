45b08a194d8b6b4707c94defae3444628b10d0b7
xonic <xonic789@naver.com>
2024-05-17 11:12:49 +0900

    방문자수 추가

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1715858887',resources: [
+    cacheName: 'chirpy-1715911941',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
