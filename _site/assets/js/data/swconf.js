fd4be330d28d7d1a6b668e3eb29d21ee3a0cdb6f
xonic <xonic789@naver.com>
2024-05-17 14:16:58 +0900

    H264 표준 코덱과구현체

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1715911941',resources: [
+    cacheName: 'chirpy-1715922994',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
