2e5d953b253397138c66083330664ecfc4427335
xonic <xonic789@naver.com>
2024-05-23 16:55:57 +0900

    Decimal 글 일부 수정

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1716450556',resources: [
+    cacheName: 'chirpy-1716450941',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
