3db74087a591f4c2f054170c8bc8787b865769ea
xonic <xonic789@naver.com>
2024-05-23 16:49:35 +0900

    Decimal

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1715923965',resources: [
+    cacheName: 'chirpy-1716450556',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
