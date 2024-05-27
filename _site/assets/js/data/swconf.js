01aae76b0892f4c6a6b2e5a1b0060af8baa690d7
xonic <xonic789@naver.com>
2024-05-27 15:39:06 +0900

    생성된 부가 파일 커밋

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1716790264',resources: [
+    cacheName: 'chirpy-1716791870',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
