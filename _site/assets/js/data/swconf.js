6587ff4d7501a48fd3da8d42a8984ea804daf1ea
xonic <xonic789@naver.com>
2024-07-15 16:01:08 +0900

    인프콘 참여

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1718009299',resources: [
+    cacheName: 'chirpy-1721026857',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
